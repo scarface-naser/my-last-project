f56eef0131b98ff42083e5e4d936c48d8931eda4
scarface naser <scarface.naser@gmail.com>
2024-12-04 16:10:04 +0300

    myfirstbrach😂

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1>version 2</h1>
+      <h1>branch 1</h1>
       <h1 className="title">Naser Start</h1>
       <h1 className="title">Jamal Start</h1>
     </>
