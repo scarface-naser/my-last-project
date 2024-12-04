b480a1f200be1cb6c64398896a1825b87c213c79
scarface naser <scarface.naser@gmail.com>
2024-12-04 16:00:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1>version 2</h1>
+      <h1 className="title">Naser Start</h1>
+      <h1 className="title">Jamal Start</h1>
+    </>
+  );
+}
+
+export default App;
